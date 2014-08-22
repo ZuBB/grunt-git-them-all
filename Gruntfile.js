5825f8f86e27171f34b369f21b63d30fa9b3872c
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-08-22 14:26:56 +0300

    Gruntfile.js: use new task name

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         },
 
         // Configuration to be run (and then tested).
-        gta: {
+        'gta': {
             default_options: {
                 options: {
                 },
